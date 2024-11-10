28a8eea2664b8783a012e742017609b27150dcd4
Avi Langburd <avi@langburd.com>
2024-11-10 14:03:42 +0200

    fix: remove comments

--- main.tf
+++ main.tf
@@ -1,8 +1,6 @@
 locals {
-  # Load the DNS zones from the zones file
   zones = yamldecode(file(var.zones_file))["dns_zones"]
 
-  # Flatten the records into a combined list
   records = flatten([
     for zone in local.zones : [
       for record_type, records_of_type in zone.records : [
@@ -19,17 +17,14 @@ locals {
     ]
   ])
 
-  # Combine the zone IDs from both created and existing zones
   zone_ids = merge(
     { for zone_name, zone in cloudflare_zone.created_zones : zone_name => zone.id },
     { for zone_name, zone in data.cloudflare_zone.existing_zones : zone_name => zone.id }
   )
 }
 
-# Get the Cloudflare account ID
 data "cloudflare_accounts" "current" {}
 
-# Data source to fetch existing DNS zones where create is false
 data "cloudflare_zone" "existing_zones" {
   for_each = {
     for zone in local.zones : zone.name => zone if !lookup(zone, "create", false)
@@ -38,7 +33,6 @@ data "cloudflare_zone" "existing_zones" {
   name = each.value.name
 }
 
-# Resource to create new DNS zones where create is true
 resource "cloudflare_zone" "created_zones" {
   for_each = {
     for zone in local.zones : zone.name => zone if lookup(zone, "create", false)
@@ -48,7 +42,6 @@ resource "cloudflare_zone" "created_zones" {
   zone       = each.value.name
 }
 
-# Resource to create DNS records
 resource "cloudflare_record" "dns_records" {
   for_each = {
     for record in local.records : "${substr(lower(record.zone), 0, 20)}_${substr(lower(record.record_type), 0, 5)}_${substr(lower(record.name), 0, 20)}_${substr(md5(record.content), 0, 19)}" => record
